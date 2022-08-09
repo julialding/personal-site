173ab39e5cc4baa1e8d2ed6e75a3b375f6f64bfc
juliading <jding713@gmail.com>
2022-08-09 16:25:00 -0700

    bare intro content

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <>
+    <section className="flex flex-row mt-20 ml-20 mr-20 p-8 gap-10">
+      <img src="https://github.com/julialding.png" className="w-20 h-20"></img>
+      <h1 className="text-6xl font-bold">Julia Ding</h1>
+    </section>
+    <section className="p-8 mb-8 ml-20 mr-20 flex-col space-y-3 w-fitcontent">
+      <p className="">Hi! 👋 I'm Julia. I'm a <a href="https://sites.google.com/lasahighschool.org/lasa">high school senior</a> from Austin, Texas passionate about computer science 👩🏻‍💻 and mathematics 📐. I'm passionate about bridging 
+        educational and gender gaps and enjoy giving back to the community! In my free time, I enjoy playing 
+        the cello 🎻, puzzling out fun math problems, competing in hackathons, and making bad puns.
+      </p>
+    </section>
+    </>
+  )
+}
