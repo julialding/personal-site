09a13b89a769f20e86ef1ba01b96f79ff0c3aec9
juliading <jding713@gmail.com>
2022-08-10 19:05:55 -0500

    basic format and filler content

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,42 @@
 export default function Home() {
   return (
     <>
-    <section className="flex flex-row mt-20 ml-20 mr-20 p-8 gap-10">
-      <img src="https://github.com/julialding.png" className="w-20 h-20"></img>
+    <section className="flex flex-row ml-20 mr-20 mt-20 p-6 gap-6">
+      <img src="../favicon.ico" className="w-20 h-20"></img>
       <h1 className="text-6xl font-bold">Julia Ding</h1>
     </section>
-    <section className="p-8 mb-8 ml-20 mr-20 flex-col space-y-3 w-fitcontent">
-      <p className="">Hi! 👋 I&apos;m Julia. I&apos;m a <a href="https://sites.google.com/lasahighschool.org/lasa">high school senior</a> from Austin, Texas passionate about computer science 👩🏻‍💻 and mathematics 📐. I&apos;m passionate about bridging 
-        educational and gender gaps and enjoy giving back to the community! In my free time, I enjoy playing 
+    <section className="p-8 mb-8 ml-20 mr-20 mb-5 flex-col space-y-3 border-2 rounded-lg border-black w-fitcontent">
+      <h1 className="text-3xl font-bold">About Me</h1>
+      <p className="">👋 Hi, I&apos;m Julia, and I love building. I&apos;m a <a href="https://sites.google.com/lasahighschool.org/lasa">high school senior</a> from 
+        Austin, Texas passionate about computer science 👩🏻‍💻 and mathematics 📐. I enjoy playing 
+        the cello 🎻, puzzling out fun math problems, competing in hackathons, and making bad puns.
+      </p>
+    </section>
+    <section className="p-8 mb-8 ml-20 mr-20 mb-5 flex-col space-y-3 border-2 rounded-lg border-black w-fitcontent">
+      <h1 className="text-3xl font-bold">Work Experience</h1>
+      <p className="">Hi, I&apos;m Julia. 👋 I love building. I&apos;m a <a href="https://sites.google.com/lasahighschool.org/lasa">high school senior</a> from 
+        Austin, Texas passionate about computer science 👩🏻‍💻 and mathematics 📐. I enjoy playing 
+        the cello 🎻, puzzling out fun math problems, competing in hackathons, and making bad puns.
+      </p>
+    </section>
+    <section className="p-8 mb-8 ml-20 mr-20 mb-5 flex-col space-y-3 border-2 rounded-lg border-black w-fitcontent">
+      <h1 className="text-3xl font-bold">Activities</h1>
+      <p className="">Hi, I&apos;m Julia. 👋 I love building. I&apos;m a <a href="https://sites.google.com/lasahighschool.org/lasa">high school senior</a> from 
+        Austin, Texas passionate about computer science 👩🏻‍💻 and mathematics 📐. I enjoy playing 
+        the cello 🎻, puzzling out fun math problems, competing in hackathons, and making bad puns.
+      </p>
+    </section>
+    <section className="p-8 mb-8 ml-20 mr-20 mb-5 flex-col space-y-3 border-2 rounded-lg border-black w-fitcontent">
+      <h1 className="text-3xl font-bold">Projects</h1>
+      <p className="">Hi, I&apos;m Julia. 👋 I love building. I&apos;m a <a href="https://sites.google.com/lasahighschool.org/lasa">high school senior</a> from 
+        Austin, Texas passionate about computer science 👩🏻‍💻 and mathematics 📐. I enjoy playing 
+        the cello 🎻, puzzling out fun math problems, competing in hackathons, and making bad puns.
+      </p>
+    </section>
+    <section className="p-8 mb-8 ml-20 mr-20 mb-5 flex-col space-y-3 border-2 rounded-lg border-black w-fitcontent">
+      <h1 className="text-3xl font-bold">Connect!</h1>
+      <p className="">Hi, I&apos;m Julia. 👋 I love building. I&apos;m a <a href="https://sites.google.com/lasahighschool.org/lasa">high school senior</a> from 
+        Austin, Texas passionate about computer science 👩🏻‍💻 and mathematics 📐. I enjoy playing 
         the cello 🎻, puzzling out fun math problems, competing in hackathons, and making bad puns.
       </p>
     </section>
